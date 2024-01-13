be8d94f75c1eaf2cd6d04ce663d64dd4757811bc
Athisruth <athisruthmsatheesh98@gmail.com>
2024-01-13 11:53:06 +0530

    "added"

--- setup.py
+++ setup.py
@@ -22,8 +22,8 @@ def get_requirements(file_path: str) -> List[str]:
 setup(
     name="mlproject",
     version="0.0.1",
-    author="athhisruth",
-    author_email="athhisruthmsatheesh98@gmail.com",
+    author="athisruth",
+    author_email="athisruthmsatheesh98@gmail.com",
     packages=find_packages(),
     install_requires=get_requirements("requirements.txt"),
 )
